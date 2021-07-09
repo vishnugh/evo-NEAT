f4325500ba2ddd57becf3a3955c7134a15900d26
jim <jim@vsiwest.com>
2021-07-10 04:11:46 +0800

    ConcurrentSkipListMaps and other concurrency experiments

---
+++ src/main/java/com/evo/NEAT/MutationKeys.kt
@@ -0,0 +1,24 @@
+package com.evo.NEAT
+
+import com.evo.NEAT.config.NEAT_Config
+import java.util.*
+
+enum class MutationKeys(val cfg: Number, val fn: ((Genome) -> Unit) = {/* no-op */ }) {
+    STEPS(NEAT_Config.STEPS),
+    PERTURB_CHANCE(NEAT_Config.PERTURB_CHANCE),
+    WEIGHT_CHANCE(NEAT_Config.WEIGHT_CHANCE),
+    WEIGHT_MUTATION_CHANCE(NEAT_Config.WEIGHT_MUTATION_CHANCE, Genome::mutateWeight),
+    NODE_MUTATION_CHANCE(NEAT_Config.NODE_MUTATION_CHANCE, { it.mutateAddConnection(false) }),
+    CONNECTION_MUTATION_CHANCE(NEAT_Config.CONNECTION_MUTATION_CHANCE, { it.mutateAddConnection(true) }),
+    BIAS_CONNECTION_MUTATION_CHANCE(NEAT_Config.BIAS_CONNECTION_MUTATION_CHANCE, Genome::mutateAddNode),
+    DISABLE_MUTATION_CHANCE(NEAT_Config.DISABLE_MUTATION_CHANCE, Genome::disableMutate),
+    ENABLE_MUTATION_CHANCE(NEAT_Config.ENABLE_MUTATION_CHANCE, Genome::enableMutate),
+    ;
+
+    companion object {
+        val mutationRates
+            get() = EnumMap<MutationKeys, Number>(MutationKeys::class.java).apply {
+                values().forEach { mutationKeys: MutationKeys -> put(mutationKeys, mutationKeys.cfg) }
+            }
+    }
+}
