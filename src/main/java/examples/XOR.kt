5f8b221a2f0ff6a4acd7bde3b516c50208be7a63
jim <jim@vsiwest.com>
2021-07-15 03:27:09 +0800

    wip

--- src/main/java/examples/XOR.kt
+++ src/main/java/examples/XOR.kt
@@ -2,7 +2,6 @@ package examples
 
 import com.evo.NEAT.*
 import com.evo.NEAT.config.Sim
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -15,23 +14,23 @@ import kotlin.random.Random
 class XOR : Environment {
     @OptIn(ObsoleteCoroutinesApi::class)
     override fun evaluateFitness(population: Iterable<Genome>) {
-        runBlocking(context) {
-            for (gene in population) launch(context) {
-                gene.fitness = DoubleArray(17) {
-                    val (i, j) = (Random.nextBoolean()) to (Random.nextBoolean())
-                    val inputs = doubleArrayOf(if (i) 1.0 else 0.0, if (j) 1.0 else 0.0)
-                    val output = gene.evaluateNetwork(inputs)
-                    val expected = if (i xor j) 1.0 else 0.0
-                    (1.0 - abs(expected - output[0]))
-                }.average().toDouble()
-            }
+        runBlocking {
+            for (gene in population)
+                launch {
+                    gene.fitness = DoubleArray(17) {
+                        val (i, j) = (Random.nextBoolean()) to (Random.nextBoolean())
+                        val inputs = doubleArrayOf(if (i) 1.0 else 0.0, if (j) 1.0 else 0.0)
+                        val output = gene.evaluateNetwork(inputs)
+                        val expected = if (i xor j) 1.0 else 0.0
+                        (1.0 - kotlin.math.abs(expected - output[0]))
+                    }.average().toDouble()
+                }
         }
     }
 
     companion object {
 
         val xor = XOR()
-        val context = Dispatchers.Main
 
         @JvmStatic
         fun main(arg0: Array<String>) {
@@ -49,13 +48,12 @@ class XOR : Environment {
                 println("TopFitness : " + topGenome.points)
 
                 if (topGenome.points > 0.94) {
-
                     println("nodes access  hit(${accesses.get() to (accesses.get() - misses.get())})/miss(${misses.get()}) ratio ${topGenome.run { accesses.toDouble() / misses.toDouble() }}")
                     println("GenomeAdjustedFitness: ${pool.calculateGenomeAdjustedFitness()}")
                     println("species : " + pool.species.size)
                     println(topGenome.toString())
                     var special: Species? = null
-                    for ((i, species) in pool.species.withIndex()) {
+                    pool.species.withIndex().forEach { (i, species) ->
                         print("species #$i size:${species.genomes.size}")
                         if (topGenome in species.genomes) {
                             special = species
@@ -65,12 +63,7 @@ class XOR : Environment {
                     }
 
 
-//                    special!!.genomes
                     pool.species = mutableListOf(special!!)
-
-//                    println(Json {
-//                        isLenient = true; allowSpecialFloatingPointValues = true;prettyPrint = false
-//                    }.encodeToString(pool))
 
                 }
 
