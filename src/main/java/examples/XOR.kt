4a5834693b3969aabbebfb06357cfe70c5b8d35d
jim <jim@vsiwest.com>
2021-07-16 01:40:15 +0800

    progressive ladder extends the number of correct guesses each time.

--- src/main/java/examples/XOR.kt
+++ src/main/java/examples/XOR.kt
@@ -1,11 +1,11 @@
 package examples
 
 import com.evo.NEAT.*
+import com.evo.NEAT.MutationKeys.*
 import com.evo.NEAT.config.Sim
 import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import java.lang.Math.abs
 import kotlin.random.Random
 
 /**
@@ -17,18 +17,20 @@ class XOR : Environment {
         runBlocking {
             for (gene in population)
                 launch {
-                    gene.fitness = DoubleArray(17) {
+                    gene.fitness = DoubleArray(fidelity) {
                         val (i, j) = (Random.nextBoolean()) to (Random.nextBoolean())
                         val inputs = doubleArrayOf(if (i) 1.0 else 0.0, if (j) 1.0 else 0.0)
                         val output = gene.evaluateNetwork(inputs)
                         val expected = if (i xor j) 1.0 else 0.0
                         (1.0 - kotlin.math.abs(expected - output[0]))
-                    }.average().toDouble()
+                    }.average()
                 }
         }
     }
 
+
     companion object {
+        var fidelity = 1
 
         val xor = XOR()
 
@@ -39,15 +41,21 @@ class XOR : Environment {
             pool.initializePool()
             var topGenome: Genome
 
+            var ladder = 0.1
 
             var generation = 0
             while (true) {
                 //pool.evaluateFitness();
                 pool.evaluateFitness(xor)
                 topGenome = pool.topGenome
-                println("TopFitness : " + topGenome.points)
+                println("TopFitness : ${topGenome.points} vs ladder: $ladder by width $fidelity")
 
-                if (topGenome.points > 0.94) {
+                if (topGenome.points > ladder) {
+                    ladder = topGenome.points
+                    if (ladder == 1.0) {
+                        ladder = .1
+                        fidelity++
+                    }
                     println("nodes access  hit(${accesses.get() to (accesses.get() - misses.get())})/miss(${misses.get()}) ratio ${topGenome.run { accesses.toDouble() / misses.toDouble() }}")
                     println("GenomeAdjustedFitness: ${pool.calculateGenomeAdjustedFitness()}")
                     println("species : " + pool.species.size)
@@ -62,6 +70,15 @@ class XOR : Environment {
                         println()
                     }
 
+
+                    topGenome.mutationRates[PERTURB_CHANCE] = 0.0
+                    topGenome.mutationRates[WEIGHT_CHANCE] = 0.0
+                    topGenome.mutationRates[WEIGHT_MUTATION_CHANCE] = 0.0
+                    topGenome.mutationRates[NODE_MUTATION_CHANCE] = 0.0
+                    topGenome.mutationRates[CONNECTION_MUTATION_CHANCE] = 0.0
+                    topGenome.mutationRates[BIAS_CONNECTION_MUTATION_CHANCE] = 0.0
+                    topGenome.mutationRates[DISABLE_MUTATION_CHANCE] = 0.0
+                    topGenome.mutationRates[ENABLE_MUTATION_CHANCE] = 0.0
 
                     pool.species = mutableListOf(special!!)
 
